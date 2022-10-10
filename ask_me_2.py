f600c0723d7016806eb950898958f141be3e0109
baalwa1998 <114565911+baalwa1998@users.noreply.github.com>
2022-10-10 17:35:23 +0300

    Delete ask_me_2.py

--- ask_me_2.py
+++
@@ -1,9 +0,0 @@
-print('Hello, what is your name mr/mrs')
-name= input("Enter your full name right here; Thank you....." )
-if  name == 'Baalwa Haman':
-
-      print('Hello', name)
- 
-else:
-
-      print('Hello stranger')      
