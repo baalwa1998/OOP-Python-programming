da9320e4acb1c8c594322fa91a0168ce27176822
baalwa1998 <114565911+baalwa1998@users.noreply.github.com>
2022-10-10 17:33:45 +0300

    Add files via upload

---
+++ ask_me.py
@@ -0,0 +1,5 @@
+
+
+print('Hello; What is your name sir/mdm?')
+name= input("Enter your full name right here; .....")
+print('How are you,', name)
---
+++ ask_me_2.py
@@ -0,0 +1,9 @@
+print('Hello, what is your name mr/mrs')
+name= input("Enter your full name right here; Thank you....." )
+if  name == 'Baalwa Haman':
+
+      print('Hello', name)
+ 
+else:
+
+      print('Hello stranger')      
